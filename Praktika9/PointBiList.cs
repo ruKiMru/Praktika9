845f798507f14fbac7eb9f434eba77acc8648452
ruKiMru <iliakazakov59@gmail.com>
2020-06-14 20:57:04 +0500

    Добавьте файлы проекта.

---
+++ Praktika9/PointBiList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praktika9
+{
+    class PointBiList
+    {
+        // Информационное поле
+        public int data;
+
+        // Адрес следующего элемента
+        public PointBiList next;
+
+        // Адрес предыдущего элемента
+        public PointBiList pred;
+
+        static private Random rand = new Random();
+
+        public PointBiList()
+        {
+            data = rand.Next(-20, 20);
+            next = null;
+            pred = null;
+        }
+
+        public PointBiList(int num)
+        {
+            data = num;
+            next = null;
+            pred = null;
+        }
+
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+        // Создание элемента списка
+        static PointBiList MakePoint(int num)
+        {
+            PointBiList p = new PointBiList(num);
+            return p;
+        }
+
+        // Создание двунаправленного списка
+        public static PointBiList MakeList(int size)
+        {
+            PointBiList beg = MakePoint(rand.Next(-20, 20));
+            for (int i = 1; i < size; i++)
+            {
+                PointBiList p = MakePoint(rand.Next(-20, 20));
+                p.next = beg;
+                beg.pred = p;
+                beg = p;
+            }
+
+            return beg;
+        }
+
+        public static void Print(PointBiList beg)
+        {
+            // Проверка наличия элементов в списке
+            if (beg == null)
+            {
+                Console.WriteLine("Список пустой");
+                return;
+            }
+
+            PointBiList p = beg;
+
+            Console.WriteLine("Список:");
+            while (p != null)
+            {
+                Console.WriteLine(p.ToString());
+                // Переход к следующему элементу
+                p = p.next;
+            }
+        }
+
+        public static int Sum(PointBiList beg)
+        {
+            int sum = 0;
+
+            PointBiList p = beg;
+            while (p != null)
+            {
+                sum += p.data;
+                // Переход к следующему элементу
+                p = p.next;
+            }
+
+            return sum;
+        }
+
+        public static int SumRec(PointBiList beg, int sum = 0)
+        {
+            PointBiList p = beg;
+            if (p != null)
+            {
+                sum += p.data;
+                // Переход к следующему элементу
+                return SumRec(p.next, sum);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Praktika9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Praktika9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите длину двунаправленного списка:");
+            int size = InputInteger(1, 100);
+            var beg = PointBiList.MakeList(size);
+
+            PointBiList.Print(beg);
+
+            Console.WriteLine("Сумма информационных полей:");
+            Console.WriteLine(PointBiList.Sum(beg));
+
+            Console.WriteLine("Сумма информационных полей через рекурсию:");
+            Console.WriteLine(PointBiList.SumRec(beg));
+        }
+
+        private static int InputInteger(int min, int max)
+        {
+            int output;
+
+            while (!int.TryParse(Console.ReadLine(), out output) ||
+                   !(output >= min && output <= max))
+            {
+                Console.WriteLine("Введите целое число от " + min + " до " + max);
+            }
+
+            return output;
+        }
+    }
+}
